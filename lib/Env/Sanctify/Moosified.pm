c51edc47855a6165ffaf84c796f62efd4d3c3304
Chris Williams <chris@bingosnet.co.uk>
2013-02-08 00:42:31 +0000

    Import of BINGOS/Env-Sanctify-Moosified-1.06 from CPAN.
    gitpan-cpan-distribution: Env-Sanctify-Moosified
    gitpan-cpan-version:      1.06
    gitpan-cpan-path:         BINGOS/Env-Sanctify-Moosified-1.06.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/Env/Sanctify/Moosified.pm
+++ lib/Env/Sanctify/Moosified.pm
@@ -1,12 +1,15 @@
 package Env::Sanctify::Moosified;
+{
+  $Env::Sanctify::Moosified::VERSION = '1.06';
+}
+
+#ABSTRACT: Lexically scoped sanctification of %ENV
 
 use strict;
 use warnings;
-use vars qw($VERSION);
 
-$VERSION = '1.04';
-
-use Moose;
+use Moo;
+use MooX::late;
 
 has env => (
   is      => 'ro',
@@ -40,7 +43,7 @@ sub consecrate {
 
 sub _sanctify {
   my $self = shift;
-  foreach my $regex ( @{ $self->sanctify } ) { 
+  foreach my $regex ( @{ $self->sanctify } ) {
     $self->_backup->{$_} = delete $ENV{$_} for grep { eval { /$regex/ } } keys %ENV;
   }
   $self->_backup->{$_} = delete $ENV{$_} for grep { defined $ENV{$_} } keys %{ $self->env };
@@ -60,16 +63,22 @@ sub DEMOLISH {
   $self->restore unless $self->_restored;
 }
 
-no Moose;
+no Moo;
 __PACKAGE__->meta->make_immutable;
 
 'Sanctify yourself, set yourself free';
 
 __END__
 
+=pod
+
 =head1 NAME
 
 Env::Sanctify::Moosified - Lexically scoped sanctification of %ENV
+
+=head1 VERSION
+
+version 1.06
 
 =head1 SYNOPSIS
 
@@ -79,7 +88,7 @@ Env::Sanctify::Moosified - Lexically scoped sanctification of %ENV
 
   $sanctify->restore
 
-  { 
+  {
 
     my $sanctify = Env::Sanctify::Moosified->consecrate( env => { POE_TRACE_DEFAULT => 1 } );
 
@@ -90,16 +99,18 @@ Env::Sanctify::Moosified - Lexically scoped sanctification of %ENV
 
 =head1 DESCRIPTION
 
-Env::Sanctify::Moosified is a module that provides lexically scoped manipulation and sanctification of 
+Env::Sanctify::Moosified is a module that provides lexically scoped manipulation and sanctification of
 C<%ENV>.
 
 You can specify that it alter or add additional environment variables or remove existing ones
 according to a list of matching regexen.
 
-You can then either C<restore> the environment back manually or let the object fall out of 
+You can then either C<restore> the environment back manually or let the object fall out of
 scope, which automagically restores.
 
 Useful for manipulating the environment that forked processes and sub-processes will inherit.
+
+=for Pod::Coverage   DEMOLISH
 
 =head1 CONSTRUCTOR
 
@@ -109,8 +120,17 @@ Useful for manipulating the environment that forked processes and sub-processes 
 
 Creates an Env::Sanctify::Moosified object. Takes two optional arguments:
 
-  'env', a hashref of env vars to add to %ENV;
-  'sanctify', an arrayref of regex pattern strings to match against current %ENV vars;
+=over
+
+=item C<env>
+
+A hashref of env vars to add to C<%ENV>.
+
+=item C<sanctify>
+
+An arrayref of regex pattern strings to match against current C<%ENV> vars;
+
+=back
 
 Any C<%ENV> var that matches a C<sanctify> regex is removed from the resultant C<%ENV>.
 
@@ -122,19 +142,20 @@ Any C<%ENV> var that matches a C<sanctify> regex is removed from the resultant C
 
 =item C<restore>
 
-Explicitly restore the previous C<%ENV>. This is called automagically when the object is C<DESTROY>ed, 
+Explicitly restore the previous C<%ENV>. This is called automagically when the object is C<DESTROY>ed,
 for instance, when it goes out of scope.
 
 =back
 
 =head1 AUTHOR
 
-Chris C<BinGOs> Williams <chris@bingosnet.co.uk>
+Chris Williams <chris@bingosnet.co.uk>
 
-=head1 LICENSE
+=head1 COPYRIGHT AND LICENSE
 
-Copyright E<copy> Chris Williams
+This software is copyright (c) 2013 by Chris Williams.
 
-This module may be used, modified, and distributed under the same terms as Perl itself. Please see the license that came with your Perl distribution for details.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
